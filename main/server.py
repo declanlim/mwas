17320db552454bd240f9be622ed8fe89c3958b6b
finesden33 <e6fine@gmail.com>
2024-06-24 15:58:36 -0400

    server update

--- main/server.py
+++ main/server.py
@@ -1,24 +1,41 @@
 """server for other users to run mwas remotely"""
 from flask import Flask, request, jsonify
+import os
 import subprocess
+from tempfile import NamedTemporaryFile
 
 app = Flask(__name__)
 
 
-@app.route('/run_script', methods=['POST'])
-def run_script():
-    """Run a Python script with the given arguments and return the output."""
-    # Get JSON data from the request
-    data = request.json
-    args = data.get('args', [])
-
-    # Construct the command
-    command = ['nohup', 'python3', 'mwas_general.py'] + args + ['>', 'log.txt', '2>&1', '&']
-
+@app.route('/run', methods=['POST'])
+def run():
+    """run mwas"""
     try:
-        # Execute the command
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        return jsonify({"message": "Script is running in the background", "pid": process.pid})
+        # Receive CSV data from request
+        csv_data = request.data.decode('utf-8')
+
+        # flags for mwas
+        flags = request.args.getlist('flag')
+
+        # Create a temporary file to store the CSV data
+        with NamedTemporaryFile(delete=False, mode='w', suffix='.csv') as temp_csv_file:
+            temp_csv_file.write(csv_data)
+
+        # Get the filepath of the temporary CSV file
+        temp_csv_filepath = temp_csv_file.name
+
+        # Prepare the subprocess command
+        args = ['arg1', 'arg2']  # Example additional arguments
+        command = ['nohup', 'python3', 'mwas_general.py', temp_csv_filepath] + flags + ['>', 'log.txt', '2>&1', '&']
+
+        # Execute the subprocess command with the temporary CSV file as an argument
+        subprocess.run(command, cwd=os.path.dirname(temp_csv_filepath), check=True, shell=True)
+
+        # After processing, remove the temporary CSV file
+        os.remove(temp_csv_filepath)
+
+        return jsonify({"message": "MWAS processed successfully"})
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
